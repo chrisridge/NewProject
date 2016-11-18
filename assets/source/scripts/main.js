b3a75072c3ede31dfd6893ce2c64238d9f91fe47
Chris Longridge <chris@longridge.eu>
2016-11-18 13:43:12 +0000

    added eu cookie law script

--- assets/source/scripts/main.js
+++ assets/source/scripts/main.js
@@ -1,7 +1,22 @@
-// http://youmightnotneedjquery.com/#ready
+// 
+function EuCookieAccept(){
+    console.log("cookie set");
+    var CurrentDateTime = "date";
+    document.cookie = "agree-to-cookies=yes";
+}
+
+// check if eu cookie law cookie exists
+function EuCookieCheck(){
+    if (document.cookie.length == 0) {
+        console.log("searching for eu cookie law cookie");
+        document.getElementById("eu-cookie-warning").style.display = 'block';
+    }
+}
+// when the page is ready - http://youmightnotneedjquery.com/#ready
 function ready(fn) {
     if (document.readyState != 'loading'){
         fn();
+        EuCookieCheck();
     } else {
         document.addEventListener('DOMContentLoaded', fn);
     }
