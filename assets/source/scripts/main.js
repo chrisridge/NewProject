9365ca9ac06495f8b8c4c78c46f9545ee7e8dba1
Chris Longridge <chris@longridge.eu>
2016-11-28 11:36:05 +0000

    Update main.js

--- assets/source/scripts/main.js
+++ assets/source/scripts/main.js
@@ -1,12 +1,12 @@
 // set eu cookie law cookie
-function EuCookieAccept(){
+function euCookieAccept(){
     console.log("cookie set");
     var CurrentDateTime = "date";
     document.cookie = "agree-to-cookies=yes; expires=Mon, 1 APR 2030 12:00:00 UTC";
 }
 
 // check if eu cookie law cookie exists
-function EuCookieCheck(){
+function euCookieCheck(){
     if (document.cookie.length == 0) {
         document.getElementById("eu-cookie-warning").style.display = 'block';
     }
@@ -24,5 +24,5 @@ function ready(fn) {
 // load scripts here
 ready(function() {
     console.log("Seems to be working.");
-    EuCookieCheck();
+    euCookieCheck();
 });
